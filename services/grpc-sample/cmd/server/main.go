0d13ba5cbb255b7be89ba4d17ba1d05747b61381
nsuzuki7713 <nsuzuki7713@gmail.com>
2022-11-09 00:03:50 +0900

    実装

--- services/grpc-sample/cmd/server/main.go
+++ services/grpc-sample/cmd/server/main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+
+	hellopb "grpc-sample/pkg/grpc"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
+
+type myServer struct {
+	hellopb.UnimplementedGreetingServiceServer
+}
+
+// 自作サービス構造体のコンストラクタを定義
+func NewMyServer() *myServer {
+	return &myServer{}
+}
+
+func (s *myServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
+	// リクエストからnameフィールドを取り出して
+	// "hello, [名前]!というレスポンスを返す"
+	return &hellopb.HelloResponse{
+		Message: fmt.Sprintf("Hello, %s!", req.GetName()),
+	}, nil
+}
 
 func main() {
 	// 1. 8080番portのリスナーを作成
@@ -19,9 +42,23 @@ func main() {
 	// 2. gRPCサーバを作成
 	s := grpc.NewServer()
 
+	// gRPCサーバーにGreetingServiceを登録
+	hellopb.RegisterGreetingServiceServer(s, NewMyServer())
+
+	// サーバーリフレクションの設定
+	// gRPCurlを使うために必要
+	reflection.Register(s)
+
 	// 3. 作成したサーバーを8080番ポートで稼働させる
 	go func() {
 		log.Printf("start grpc server port: %v", port)
 		s.Serve(listener)
 	}()
+
+	// 4. Ctrl+Cが入力されたらGraceful shutdownされるようにする。
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("stopping gRPC server...")
+	s.GracefulStop()
 }
