8bc66abac56a8eda8a75fec1bbe57b9061d3b0cd
nsuzuki7713 <nsuzuki7713@gmail.com>
2022-11-28 01:37:02 +0900

    追加

--- services/playground/cmd/algorithm/main.go
+++ services/playground/cmd/algorithm/main.go
@@ -10,3 +10,40 @@ func LinearSearch(nums []int, value int) int {
 
 	return -1
 }
+
+// 二分探索
+func BinarySearch(nums []int, value int) int {
+	low := 0
+	high := len(nums) - 1
+
+	for low <= high {
+		mid := (low + high) / 2
+
+		if nums[mid] == value {
+			return mid
+		}
+		if nums[mid] < value {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+
+	return -1
+}
+
+// バブルソート
+// 隣り合う要素の大小を比較しながら整列させるソートアルゴリズム。
+func BubbleSort(nums []int) []int {
+	lenNums := len(nums)
+
+	for i := 0; i < lenNums; i++ {
+		for j := 0; j < lenNums-1-i; j++ {
+			if nums[j] > nums[j+1] {
+				nums[j], nums[j+1] = nums[j+1], nums[j]
+			}
+		}
+	}
+
+	return nums
+}
--- services/playground/cmd/algorithm/main_test.go
+++ services/playground/cmd/algorithm/main_test.go
@@ -1,6 +1,9 @@
 package algorithm
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestLinearSearch(t *testing.T) {
 	nums := []int{1, 4, 3, 7, 4, 6, 10}
@@ -11,5 +14,27 @@ func TestLinearSearch(t *testing.T) {
 
 	if LinearSearch(nums, 100) != -1 {
 		t.Errorf("LinearSearch() = %v, want %v", LinearSearch(nums, 100), -1)
+	}
+}
+
+func TestBinarySearch(t *testing.T) {
+	nums := []int{1, 3, 7, 12, 24, 36, 37, 40}
+
+	if BinarySearch(nums, 7) != 2 {
+		t.Errorf("BinarySearch() = %v, want %v", BinarySearch(nums, 7), 2)
+	}
+
+	if BinarySearch(nums, 13) != -1 {
+		t.Errorf("BinarySearch() = %v, want %v", BinarySearch(nums, 100), -1)
+	}
+}
+
+func TestBubbleSort(t *testing.T) {
+	nums := []int{1, 2, 4, 3, 7, 6, 5, 8, 9}
+
+	expect := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	if r := BubbleSort(nums); !reflect.DeepEqual(r, expect) {
+		t.Errorf("error")
 	}
 }
